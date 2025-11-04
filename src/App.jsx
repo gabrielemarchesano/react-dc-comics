a639d9e6dfcc3cf6fe078b856855ba731273d295
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-04 16:43:45 +0100

    main: create middle content section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import dcLogo from "./assets/img/dc-logo.png"
+import dcDigital from "./assets/img/buy-comics-digital-comics.png"
+import dcMerchandise from "./assets/img/buy-comics-merchandise.png"
+import dcSubscription from "./assets/img/buy-comics-subscriptions.png"
+import dcLocation from "./assets/img/buy-comics-shop-locator.png"
+import dcVisa from "./assets/img/buy-dc-power-visa.svg"
 
 function App() {
 
   return (
-    <div className="container">
-      <header className="d-flex justify-content-between py-3">
+    <div>
+      <header className="d-flex justify-content-between py-3 container">
         <div className="logo">
           <img src={dcLogo} alt="" />
         </div>
@@ -22,19 +27,39 @@ function App() {
         </div>
       </header>
       <main>
-        <section className="content">
-          <div className="upper-content">
-            <p> --{">"} Content goes here {"<"}-- </p>
-          </div>
-          <div className="bottom-content">
-            <div className="row">
-              <div className="col"></div>
-              <div className="col"></div>
-              <div className="col"></div>
-              <div className="col"></div>
-              <div className="col"></div>
+        <section className="content text-white">
+          <div className="upper-content bg-dark py-5">
+            <div className="container">
+              <span> --{">"} Content goes here {"<"}-- </span>
             </div>
           </div>
+          <div className="middle-content bg-primary py-5">
+            <div className="container">
+              <div className="row justify-content-around">
+                <div className="col">
+                  <img className="shopping-images" src={dcDigital} alt="" />
+                  <span>DIGITAL COMICS</span>
+                </div>
+                <div className="col">
+                  <img className="shopping-images" src={dcMerchandise} alt="" />
+                  <span>DC MERCHANDISE</span>
+                </div>
+                <div className="col">
+                  <img className="shopping-images" src={dcSubscription} alt="" />
+                  <span>SUBSCRIPTION</span>
+                </div>
+                <div className="col">
+                  <img className="shopping-images" src={dcLocation} alt="" />
+                  <span>COMIC SHOP LOCATOR</span>
+                </div>
+                <div className="col">
+                  <img className="shopping-images" src={dcVisa} alt="" />
+                  <span>DC POWER VISA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          
         </section>
       </main>
       <footer>
