0991dca7567c1310cc4ae78ff857b0cc7cbd8eaf
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-04 15:18:05 +0100

    create main structure; create header, add logo and navbar without components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import dcLogo from "./assets/img/dc-logo.png"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <div className="container">
+      <header className="d-flex justify-content-between">
+        <div className="logo">
+          <img src={dcLogo} alt="" />
+        </div>
+        <div className="navbar gap-3 p-0">
+          <a href="">CHARACTERS</a>
+          <a href="">COMICS</a>
+          <a href="">MOVIES</a>
+          <a href="">TV</a>
+          <a href="">GAMES</a>
+          <a href="">COLECTIBLES</a>
+          <a href="">VIDEOS</a>
+          <a href="">FANS</a>
+          <a href="">NEWS</a>
+          <a href="">SHOP</a>
+        </div>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
   )
 }
 
