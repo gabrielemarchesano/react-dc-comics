81c06486bd8e30473d76b3e4ac744ec1bb19b970
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-05 17:11:16 +0100

    navbar: add structure for data information for the links in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,65 @@
 export default function Navbar(){
+  
+  const navbar = [
+    {
+      id: 1,
+      link: "#",
+      content: "CHARACTERS"
+    },
+    {
+      id: 2,
+      link: "#",
+      content: "COMICS"
+    },
+    {
+      id: 3,
+      link: "#",
+      content: "MOVIES"
+    },
+    {
+      id: 4,
+      link: "#",
+      content: "TV"
+    },
+    {
+      id: 5,
+      link: "#",
+      content: "GAMES"
+    },
+    {
+      id: 6,
+      link: "#",
+      content: "COLLECTIBLES"
+    },
+    {
+      id: 7,
+      link: "#",
+      content: "VIDEOS"
+    },
+    {
+      id: 8,
+      link: "#",
+      content: "FANS"
+    },
+    {
+      id: 9,
+      link: "#",
+      content: "NEWS"
+    },
+    {
+      id: 10,
+      link: "#",
+      content: "SHOP"
+    }
+  ]
+
   return(
     <div className="navbar gap-4 p-0">
-      <a className="pages" href="">CHARACTERS</a>
-      <a className="pages" href="">COMICS</a>
-      <a className="pages" href="">MOVIES</a>
-      <a className="pages" href="">TV</a>
-      <a className="pages" href="">GAMES</a>
-      <a className="pages" href="">COLECTIBLES</a>
-      <a className="pages" href="">VIDEOS</a>
-      <a className="pages" href="">FANS</a>
-      <a className="pages" href="">NEWS</a>
-      <a className="pages" href="">SHOP</a>
+      {
+        navbar.map((item) => (
+          <a className="pages" key={item.id} href={item.link}>{item.content}</a>
+        ))
+      }
     </div>
   )
 }
