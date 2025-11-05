4f9e02a6abaea55f1f375c65d17d75f20834d9de
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-05 12:22:28 +0100

    footer: create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import biggerLogo from "../src/assets/img/dc-logo-bg.png"
-import facebookLogo from "../src/assets/img/footer-facebook.png"
-import twitterLogo from "../src/assets/img/footer-twitter.png"
-import youtubeLogo from "../src/assets/img/footer-youtube.png"
-import pinterestLogo from "../src/assets/img/footer-pinterest.png"
-import periscopeLogo from "../src/assets/img/footer-periscope.png"
-
 import DCMerchandise from "./components/DCMerchandise"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 function App() {
@@ -26,79 +20,7 @@ function App() {
           </div>
         </section>
       </main>
-      <footer className="text-white">
-          <div className="upper-footer py-5">
-            <div className="container">
-              <div className="row">
-                <div className="col-6">
-                  <div className="row g-1">
-                    <div className="col-4">
-                      <ul className="list-unstyled">
-                        <a href="" className="list-group-item list-title">DC COMICS</a>
-                        <a href="" className="list-group-item">Characters</a>
-                        <a href="" className="list-group-item">Comics</a>
-                        <a href="" className="list-group-item">Movies</a>
-                        <a href="" className="list-group-item">TV</a>
-                        <a href="" className="list-group-item">Games</a>
-                        <a href="" className="list-group-item">Videos</a>
-                        <a href="" className="list-group-item">News</a>
-                      </ul>
-                      <ul className="list-unstyled">
-                        <a href="" className="list-group-item list-title">DC COMICS</a>
-                        <a href="" className="list-group-item">Characters</a>
-                        <a href="" className="list-group-item">Comics</a>
-                      </ul>
-                    </div>
-                    <div className="col-4">
-                      <ul className="list-unstyled">
-                        <a href="" className="list-group-item list-title">DC COMICS</a>
-                        <a href="" className="list-group-item">Characters</a>
-                        <a href="" className="list-group-item">Comics</a>
-                        <a href="" className="list-group-item">Movies</a>
-                        <a href="" className="list-group-item">TV</a>
-                        <a href="" className="list-group-item">Games</a>
-                        <a href="" className="list-group-item">Videos</a>
-                        <a href="" className="list-group-item">News</a>
-                        <a href="" className="list-group-item">Videos</a>
-                        <a href="" className="list-group-item">News</a>
-                        <a href="" className="list-group-item">Videos</a>
-                        <a href="" className="list-group-item">News</a>
-                      </ul>
-                    </div>
-                    <div className="col-4">
-                      <ul className="list-unstyled">
-                        <a href="" className="list-group-item list-title">DC COMICS</a>
-                        <a href="" className="list-group-item">Characters</a>
-                        <a href="" className="list-group-item">Comics</a>
-                        <a href="" className="list-group-item">Movies</a>
-                        <a href="" className="list-group-item">TV</a>
-                        <a href="" className="list-group-item">Games</a>
-                        <a href="" className="list-group-item">Videos</a>
-                        <a href="" className="list-group-item">News</a>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-6">
-                  <img id="bigger-logo" src={biggerLogo} alt="" srcset="" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="bottom-footer py-5 bg-dark">
-            <div className="container d-flex justify-content-between">
-              <button type="button" className="sign-up-button btn btn-primary border-2 py-3">SIGN-UP NOW!</button>
-              <div className="d-flex align-items-center gap-3">
-                <span className="mx-2">FOLLOW US</span>
-                <img src={facebookLogo} alt="" />
-                <img src={twitterLogo} alt="" />
-                <img src={youtubeLogo} alt="" />
-                <img src={pinterestLogo} alt="" />
-                <img src={periscopeLogo} alt="" />
-              </div>
-            </div>
-          </div>
-      </footer>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,84 @@
+import biggerLogo from "../assets/img/dc-logo-bg.png"
+import facebookLogo from "../assets/img/footer-facebook.png"
+import twitterLogo from "../assets/img/footer-twitter.png"
+import youtubeLogo from "../assets/img/footer-youtube.png"
+import pinterestLogo from "../assets/img/footer-pinterest.png"
+import periscopeLogo from "../assets/img/footer-periscope.png"
+
+export default function Footer(){
+  return(
+    <footer className="text-white">
+      <div className="upper-footer py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <div className="row g-1">
+                <div className="col-4">
+                  <ul className="list-unstyled">
+                    <a href="" className="list-group-item list-title">DC COMICS</a>
+                    <a href="" className="list-group-item">Characters</a>
+                    <a href="" className="list-group-item">Comics</a>
+                    <a href="" className="list-group-item">Movies</a>
+                    <a href="" className="list-group-item">TV</a>
+                    <a href="" className="list-group-item">Games</a>
+                    <a href="" className="list-group-item">Videos</a>
+                    <a href="" className="list-group-item">News</a>
+                  </ul>
+                  <ul className="list-unstyled">
+                    <a href="" className="list-group-item list-title">DC COMICS</a>
+                    <a href="" className="list-group-item">Characters</a>
+                    <a href="" className="list-group-item">Comics</a>
+                  </ul>
+                </div>
+                <div className="col-4">
+                  <ul className="list-unstyled">
+                    <a href="" className="list-group-item list-title">DC COMICS</a>
+                    <a href="" className="list-group-item">Characters</a>
+                    <a href="" className="list-group-item">Comics</a>
+                    <a href="" className="list-group-item">Movies</a>
+                    <a href="" className="list-group-item">TV</a>
+                    <a href="" className="list-group-item">Games</a>
+                    <a href="" className="list-group-item">Videos</a>
+                    <a href="" className="list-group-item">News</a>
+                    <a href="" className="list-group-item">Videos</a>
+                    <a href="" className="list-group-item">News</a>
+                    <a href="" className="list-group-item">Videos</a>
+                    <a href="" className="list-group-item">News</a>
+                  </ul>
+                </div>
+                <div className="col-4">
+                  <ul className="list-unstyled">
+                    <a href="" className="list-group-item list-title">DC COMICS</a>
+                    <a href="" className="list-group-item">Characters</a>
+                    <a href="" className="list-group-item">Comics</a>
+                    <a href="" className="list-group-item">Movies</a>
+                    <a href="" className="list-group-item">TV</a>
+                    <a href="" className="list-group-item">Games</a>
+                    <a href="" className="list-group-item">Videos</a>
+                    <a href="" className="list-group-item">News</a>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <img id="bigger-logo" src={biggerLogo} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-footer py-5 bg-dark">
+        <div className="container d-flex justify-content-between">
+          <button type="button" className="sign-up-button btn btn-primary border-2 py-3">SIGN-UP NOW!</button>
+          <div className="d-flex align-items-center gap-3">
+            <span className="mx-2">FOLLOW US</span>
+            <img src={facebookLogo} alt="" />
+            <img src={twitterLogo} alt="" />
+            <img src={youtubeLogo} alt="" />
+            <img src={pinterestLogo} alt="" />
+            <img src={periscopeLogo} alt="" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
