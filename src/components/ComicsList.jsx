be989694c53fb967ad4be5ebc534abf88e54a022
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-05 12:57:10 +0100

    comics-list: create list with comics

--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -60,7 +60,7 @@ export default function ComicsList() {
       id: 5,
       title: "Batman #56",
       description: "The Dark Knight's looking to drop both the hammer and sickle on the KGBeast. The Russian super-assassin has gone too far, and Batman will stop at nothing to hunt him down. But is the Dark Knight willing to step into the darkness himself to find justice?",
-      thumb: "https://imgs.search.brave.com/3vP5d3cOVOuC0f6Uhm7CkV_qFk5hGj5rE4jK5I6n7dI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4y/LnBlbmd1aW4uY29t/LmF1L2NvdmVycy9v/cmlnaW5hbC83OTYx/OTQzMTM5OTk4Lmpw/Zw",
+      thumb: "https://imgs.search.brave.com/jgxYlrx442aozemzjqdmCsd9DkIkRollJp1T8sG8TfE/rs:fit:720:1106:1/g:ce/aHR0cHM6Ly9veXN0/ZXIuaWduaW1ncy5j/b20vd29yZHByZXNz/L3N0Zy5pZ24uY29t/LzIwMTgvMTAvU1RM/MDk3MDk1LTcyMHgx/MTA2LmpwZw",
       price: "$3.99",
       series: "Batman",
       sale_date: "2018-10-03",
@@ -157,7 +157,20 @@ export default function ComicsList() {
     <div>
       <div className="upper-content bg-dark py-5">
         <div className="container">
-          <span> --{">"} Content goes here {"<"}-- </span>
+          <div className="row g-3">
+            {
+              comics.map((comic) => (
+                <div className="col" key={comic.id}>
+                  <div className="card">
+                    <img src={comic.thumb} alt="" className="card-img-top" />
+                    <div className="card-body">
+                      <h3>{comic.series}</h3>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
